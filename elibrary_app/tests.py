ec11e4ae191468989e039ea43e1add7b29725a72
radostev <pavel.radostev@zyfra.com>
2024-06-01 08:24:00 +0700

    Add possibility to add a new book to the library

--- elibrary_app/tests.py
+++ elibrary_app/tests.py
@@ -6,7 +6,34 @@ from .views import home
 from .forms import AddBookForm
 
 
-class CatalogTemplateTests(SimpleTestCase):
+class CatalogViewTests(TestCase):
+    """    Тест для представлений    """
+    def test_book_list_view(self):
+
+        Book_1 = Catalog.objects.create(
+            title='Django for Beginners (2018)',
+            ISBN='978-1-60309-0',
+            author='John Doe',
+            price=9.99,
+            availability='true'
+        )
+
+        Book_2 = Catalog.objects.create(
+            title='Django for Professionals (2020)',
+            ISBN='978-1-60309-3',
+            author='Mary Doe',
+            price=11.99,
+            availability='false'
+        )
+
+        response = self.client.get(reverse('home'))
+
+        self.assertIn('Django for Professionals (2020)', response.content.decode())
+        self.assertIn('John Doe', response.content.decode())
+        self.assertIn('978-1-60309-3', response.content.decode())
+
+
+class CatalogTemplateTests(TestCase):
     """    Тест шаблона    """
     def test_homepage_template(self):
         response = self.client.get(reverse('home'))
@@ -21,7 +48,7 @@ class CatalogTemplateTests(SimpleTestCase):
         self.assertNotContains(response, 'Hello World')
 
 
-class CatalogFormTests(SimpleTestCase):
+class CatalogFormTests(TestCase):
     """    Тесты для форм    """
     def setUp(self):
         url = reverse('home')
@@ -43,7 +70,7 @@ class CatalogFormTests(SimpleTestCase):
         self.assertFalse(add_book_form.is_valid())
 
 
-class ElibraryURLsTest(SimpleTestCase):
+class ElibraryURLsTest(TestCase):
     """    Тестируем URLs    """
 
     def test_homepage_url_name(self):
