f9e47032b7594dbc32cc8b8fdb0436e51b1787f5
radostev <pavel.radostev@zyfra.com>
2024-06-11 18:43:16 +0700

    Add book delete

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    book_list, create_book, update_book_details, book_detail
+    book_list, create_book, update_book_details, book_detail, delete_book
 )
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("create_book/", create_book, name="create_book"),
     path("update_book_details/<int:pk>/", update_book_details, name="update_book_details"),
     path("book_detail/<int:pk>/", book_detail, name="book_detail"),
+    path("delete_book/<int:pk>/", delete_book, name="delete_book"),
 ]
